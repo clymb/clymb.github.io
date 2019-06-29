ddcfeef649cd4a8f3c75439a9d34b7aaa91e05f7
chethan177 <chethan177@gmail.com>
2019-06-29 10:16:10 +0530

    Fix mobile display for user guide

--- guides/js/jquery.nav.js
+++ guides/js/jquery.nav.js
@@ -15,16 +15,90 @@ if (typeof Object.create !== 'function') {
     };
 }
 
+// Mobile Navigation
+if ($('#left-sidebar').length) {
+    var $mobile_nav = $('#left-sidebar').clone().prop({
+        id: 'mobile-nav'
+    });
+    $mobile_nav.find('> ul').attr({
+        'class': '',
+        'id': ''
+    });
+    $('body').append($mobile_nav);
+    $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="lnr lnr-menu"></i></button>');
+    $('body').append('<div id="mobile-body-overly"></div>');
+    $('#mobile-nav').find('.menu-has-children').prepend('<i class="lnr lnr-chevron-down"></i>');
+
+    $(document).on('click', '.menu-has-children i', function (e) {
+        $(this).next().toggleClass('menu-item-active');
+        $(this).nextAll('ul').eq(0).slideToggle();
+        $(this).toggleClass("lnr-chevron-up lnr-chevron-down");
+    });
+
+    $(document).on('click', '#mobile-nav-toggle', function (e) {
+        $('body').toggleClass('mobile-nav-active');
+        $('#mobile-nav-toggle i').toggleClass('lnr-cross lnr-menu');
+        $('#mobile-body-overly').toggle();
+    });
+
+    $(document).click(function (e) {
+        var container = $("#mobile-nav, #mobile-nav-toggle");
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            if ($('body').hasClass('mobile-nav-active')) {
+                $('body').removeClass('mobile-nav-active');
+                $('#mobile-nav-toggle i').toggleClass('lnr-cross lnr-menu');
+                $('#mobile-body-overly').fadeOut();
+            }
+        }
+    });
+} else if ($("#mobile-nav, #mobile-nav-toggle").length) {
+    $("#mobile-nav, #mobile-nav-toggle").hide();
+}
+
+// Smooth scroll for the menu and links with .scrollto classes
+$('.left-menu a, #mobile-nav a, .scrollto').on('click', function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        if (target.length) {
+            var top_space = 0;
+
+            if ($('#header').length) {
+                top_space = $('#header').outerHeight();
+
+                if (!$('#header').hasClass('header-fixed')) {
+                    top_space = top_space;
+                }
+            }
+
+            $('html, body').animate({
+                scrollTop: target.offset().top - top_space
+            }, 1500, 'easeInOutExpo');
+
+            if ($(this).parents('.left-menu').length) {
+                $('.left-menu .menu-active').removeClass('menu-active');
+                $(this).closest('li').addClass('menu-active');
+            }
+
+            if ($('body').hasClass('mobile-nav-active')) {
+                $('body').removeClass('mobile-nav-active');
+                $('#mobile-nav-toggle i').toggleClass('lnr-times lnr-bars');
+                $('#mobile-body-overly').fadeOut();
+            }
+            return false;
+        }
+    }
+});
+
 (function($, window, document, undefined) {
     "use strict";
-    
+
     var SinglePageNav = {
-        
+
         init: function(options, container) {
-            
+
             this.options = $.extend({}, $.fn.singlePageNav.defaults, options);
-            
-            this.container = container;            
+
+            this.container = container;
             this.$container = $(container);
             this.$links = this.$container.find('a');
 
@@ -34,7 +108,7 @@ if (typeof Object.create !== 'function') {
 
             this.$window = $(window);
             this.$htmlbody = $('html, body');
-            
+
             this.$links.on('click.singlePageNav', $.proxy(this.handleClick, this));
 
             this.didScroll = false;
@@ -47,7 +121,7 @@ if (typeof Object.create !== 'function') {
                 link  = e.currentTarget,
                 $elem = $(link.hash);
 
-            e.preventDefault();             
+            e.preventDefault();
 
             if ($elem.length) { // Make sure the target elem exists
 
@@ -60,8 +134,8 @@ if (typeof Object.create !== 'function') {
                 }
 
                 self.setActiveLink(link.hash);
-                
-                self.scrollTo($elem, function() { 
+
+                self.scrollTo($elem, function() {
 
                     if (self.options.updateHash && history.pushState) {
                         history.pushState(null,null, link.hash);
@@ -73,20 +147,20 @@ if (typeof Object.create !== 'function') {
                     if (typeof self.options.onComplete === 'function') {
                         self.options.onComplete();
                     }
-                });                            
-            }     
+                });
+            }
         },
-        
+
         scrollTo: function($elem, callback) {
             var self = this;
             var target = self.getCoords($elem).top;
             var called = false;
 
             self.$htmlbody.stop().animate(
-                {scrollTop: target}, 
-                { 
+                {scrollTop: target},
+                {
                     duration: self.options.speed,
-                    easing: self.options.easing, 
+                    easing: self.options.easing,
                     complete: function() {
                         if (typeof callback === 'function' && !called) {
                             callback();
@@ -96,28 +170,28 @@ if (typeof Object.create !== 'function') {
                 }
             );
         },
-        
+
         setTimer: function() {
             var self = this;
-            
+
             self.$window.on('scroll.singlePageNav', function() {
                 self.didScroll = true;
             });
-            
+
             self.timer = setInterval(function() {
                 if (self.didScroll) {
                     self.didScroll = false;
                     self.checkPosition();
                 }
             }, 250);
-        },        
-        
+        },
+
         clearTimer: function() {
             clearInterval(this.timer);
             this.$window.off('scroll.singlePageNav');
             this.didScroll = false;
         },
-        
+
         // Check the scroll position and set the active section
         checkPosition: function() {
             var scrollPos = this.$window.scrollTop();
@@ -125,50 +199,50 @@ if (typeof Object.create !== 'function') {
             if(currentSection!==null) {
                 this.setActiveLink(currentSection);
             }
-        },        
-        
+        },
+
         getCoords: function($elem) {
             return {
                 top: Math.round($elem.offset().top) - this.options.offset
             };
         },
-        
+
         setActiveLink: function(href) {
             var $activeLink = this.$container.find("a[href$='" + href + "']");
-                            
+
             if (!$activeLink.hasClass(this.options.currentClass)) {
                 this.$links.removeClass(this.options.currentClass);
                 $activeLink.addClass(this.options.currentClass);
             }
-        },        
-        
+        },
+
         getCurrentSection: function(scrollPos) {
             var i, hash, coords, section;
-            
+
             for (i = 0; i < this.$links.length; i++) {
                 hash = this.$links[i].hash;
-                
+
                 if ($(hash).length) {
                     coords = this.getCoords($(hash));
-                    
+
                     if (scrollPos >= coords.top - this.options.threshold) {
                         section = hash;
                     }
                 }
             }
-            
+
             // The current section or the first link if it is found
             return section || ((this.$links.length===0) ? (null) : (this.$links[0].hash));
         }
     };
-    
+
     $.fn.singlePageNav = function(options) {
         return this.each(function() {
             var singlePageNav = Object.create(SinglePageNav);
             singlePageNav.init(options, this);
         });
     };
-    
+
     $.fn.singlePageNav.defaults = {
         offset: 0,
         threshold: 120,
@@ -180,5 +254,5 @@ if (typeof Object.create !== 'function') {
         onComplete: false,
         beforeStart: false
     };
-    
+
 })(jQuery, window, document);
